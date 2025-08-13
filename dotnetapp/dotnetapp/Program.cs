b9ad4479292e35ea4e385080c8bab6e7e860b84d
Soubhagyaranjan Bisoi <soubhagyaranjan.bisoi99@gmail.com>
2025-08-13 23:30:46 +0530

    configured-swagger

--- dotnetapp/dotnetapp/Program.cs
+++ dotnetapp/dotnetapp/Program.cs
@@ -5,8 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
@@ -22,7 +23,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
